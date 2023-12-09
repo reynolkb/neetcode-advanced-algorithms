f813375acda220e6b07f9c7230bacb6c14fc620d
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2023-12-09 09:36:17 -0800

    adding max_subarray_sum_circular

--- 1. Arrays/1. Kadane's Algorithm/2. Maximum Sum Circular Subarray/maximum_sum_circular_subarray.py
+++ 1. Arrays/1. Kadane's Algorithm/2. Maximum Sum Circular Subarray/maximum_sum_circular_subarray.py
@@ -4,11 +4,25 @@ from typing import List
 class Solution:
     @staticmethod
     def max_subarray_sum_circular(nums: List[int]) -> int:
-        print("hello")
+        glob_max, glob_min = nums[0], nums[0]
+        cur_max, cur_min = 0, 0
+        total = 0
+
+        for n in nums:
+            cur_max = max(cur_max + n, n)
+            cur_min = min(cur_min + n, n)
+            total += n
+            glob_max = max(cur_max, glob_max)
+            glob_min = min(cur_min, glob_min)
+
+        # total --> all of the elements in the array
+        # global_min --> minimum sum of subarray
+        # total - global_min --> max sum when the sub_array wraps around
+        return max(glob_max, total - glob_min) if glob_max > 0 else glob_max
 
 
 def main():
-    nums = [1, -2, 3, -2]
+    nums = [5, -3, 5]
     solution = Solution()
     result = solution.max_subarray_sum_circular(nums)
     print("Output:", result)
