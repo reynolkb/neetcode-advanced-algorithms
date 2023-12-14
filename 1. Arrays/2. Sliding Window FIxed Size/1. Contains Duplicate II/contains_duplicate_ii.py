a1f43845625b1761ea829febe86b904cec8f0039
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2023-12-13 17:51:31 -0800

    adding contains_nearby_duplicates

--- 1. Arrays/2. Sliding Window FIxed Size/1. Contains Duplicate II/contains_duplicate_ii.py
+++ 1. Arrays/2. Sliding Window FIxed Size/1. Contains Duplicate II/contains_duplicate_ii.py
@@ -4,12 +4,26 @@ from typing import List
 class Solution:
     @staticmethod
     def contains_nearby_duplicate(nums: List[int], k: int) -> bool:
-        print("hello world")
+        window = set()
+        left = 0
+
+        for right in range(len(nums)):
+            if k < right - left:
+                window.remove(nums[left])
+                left += 1
+            if nums[right] in window:
+                return True
+            window.add(nums[right])
+        return False
 
 
 def main():
-    nums = [1, 2, 3, 1]
-    k = 3
+    nums = [1, 2, 3, 1, 2, 3]
+    k = 2
     solution = Solution()
     result = solution.contains_nearby_duplicate(nums, k)
     print("Output:", result)
+
+
+if __name__ == "__main__":
+    main()
