353b4f09c6aa7a10c86cd825cd92f23d944dcc8b
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2023-12-14 08:32:42 -0800

    updates

--- 1. Arrays/2. Sliding Window Fixed Size/1. Contains Duplicate II/README.md
+++ 1. Arrays/2. Sliding Window Fixed Size/1. Contains Duplicate II/README.md
@@ -13,6 +13,10 @@ Example 2:\
 Input: `nums = [1,0,1,1], k = 1`\
 Output: `true`
 
+Example 3:\
+Input: `nums = [1,2,3,1,2,3], k = 2`\
+Output: `false`
+
 ## Run Command
 
 `poetry run python "1. Arrays/2. Sliding Window Fixed Size/1. Contains Duplicate II/contains_duplicate_ii.py"`
--- 1. Arrays/2. Sliding Window Fixed Size/2. Number of Sub Arrays of Size K and Avg Greater than or Equal to Threshold/number_of_sub_arrays_of_size_k_and_avg_greater_than_or_equal_to_threshold.py
+++ 1. Arrays/2. Sliding Window Fixed Size/2. Number of Sub Arrays of Size K and Avg Greater than or Equal to Threshold/number_of_sub_arrays_of_size_k_and_avg_greater_than_or_equal_to_threshold.py
@@ -3,14 +3,22 @@ from typing import List
 
 class Solution:
     @staticmethod
-    def num_of_subarrays(self, arr: List[int], k: int, threshold: int) -> int:
-        print("Hello World")
+    def num_of_subarrays(arr: List[int], k: int, threshold: int) -> int:
+        res = 0
+        cur_sum = sum(arr[:k - 1])
+
+        for L in range(len(arr) - k + 1):
+            cur_sum += arr[L + k - 1]
+            if (cur_sum / k) >= threshold:
+                res += 1
+            cur_sum -= arr[L]
+        return res
 
 
 def main():
-    arr = [2, 2, 2, 2, 5, 5, 5, 8]
+    arr = [11, 13, 17, 23, 29, 31, 7, 5, 2, 3]
     k = 3
-    threshold = 4
+    threshold = 5
     solution = Solution()
     result = solution.num_of_subarrays(arr, k, threshold)
     print("Output:", result)
